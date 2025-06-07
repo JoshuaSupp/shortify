c9c0bf191e6feb1af36c4dc19c66c6a91afcd3c6
JoshuaSupp <89426183+JoshuaSupp@users.noreply.github.com>
2025-06-07 19:15:48 +0530

    vercel deployment

--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-p)^!jd4n$7h_942*w!euvl+**@h-o*^o)n_es%+dw8jufg#nn*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
